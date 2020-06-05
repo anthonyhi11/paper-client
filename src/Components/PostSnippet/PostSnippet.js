873130a72223e2648a8951341c5360fe97b81211
Anthony Hill <antdavhill@gmail.com>
2020-06-05 17:40:45 -0500

    Changes to readme and postsnippet to appear different on admin page vs normal view

--- src/Components/PostSnippet/PostSnippet.js
+++ src/Components/PostSnippet/PostSnippet.js
@@ -4,16 +4,33 @@ import { Link } from "react-router-dom";
 
 export default function PostSnippet(props) {
   return (
-    <div className="post-snippet-container">
-      <h2>{props.title}</h2>
-      <h3>{props.date}</h3>
-      <p>
-        Minim ea ex dolor labore est ut non nulla proident dolor. Fugiat Lorem
-        ea laboris cillum amet. Ullamco anim duis nulla fugiat. Laboris quis
-        adipisicing ipsum adipisicing sunt esse qui. Minim officia laboris ipsum
-        excepteur reprehenderit exercitation in non commodo exercitation.
-      </p>
-      <Link to={`posts/${props.id}`}>See More</Link>
-    </div>
+    <>
+      <div
+        className={
+          !props.admin ? "post-snippet-container" : "admin-post-snippet"
+        }
+      >
+        <h2>{props.title}</h2>
+        <h3>{props.date}</h3>
+        {!props.admin && (
+          <>
+            <p>
+              Minim ea ex dolor labore est ut non nulla proident dolor. Fugiat
+              Lorem ea laboris cillum amet. Ullamco anim duis nulla fugiat.
+              Laboris quis adipisicing ipsum adipisicing sunt esse qui. Minim
+              officia laboris ipsum excepteur reprehenderit exercitation in non
+              commodo exercitation.
+            </p>
+            <Link to={`posts/${props.id}`}>See More</Link>
+          </>
+        )}
+        {props.admin && (
+          <div>
+            <p>View</p>
+            <p>Edit</p>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
--- src/Pages/Admin/AdminMain/AdminMain.js
+++ src/Pages/Admin/AdminMain/AdminMain.js
@@ -1,9 +1,11 @@
 import React from "react";
+import PostSnippet from "../../../Components/PostSnippet/PostSnippet";
 
 export default function AdminMain() {
   return (
-    <div>
+    <div className="App">
       <h1>THIS IS ADMIN PAGE</h1>
+      <PostSnippet title="Job Hunting" date="6/2/2020" admin="true" />
     </div>
   );
 }
